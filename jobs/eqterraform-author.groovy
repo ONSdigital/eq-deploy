44a7c895f91376575d2d457c81bc70c98aadc0ac
Warren Bailey <warren@warrenbailey.net>
2016-06-03 09:27:53 +0100

    Adding new job for Author terraform

--- jobs/eqterraform-author.groovy
+++ jobs/eqterraform-author.groovy
@@ -7,8 +7,8 @@ job('Terraform Environment') {
        githubPush()
   }
   steps {
-    shell('cd survey-runner')
-    shell('terraform remote config -backend=S3 -backend-config="bucket=jenkins-ci-production-terraform-state" -backend-config="key=jenkins" -backend-config="region=eu-west-1"')
+    shell('cd author')
+    shell('terraform remote config -backend=S3 -backend-config="bucket=jenkins-ci-production-author-terraform-state" -backend-config="key=jenkins" -backend-config="region=eu-west-1"')
     shell('terraform remote pull')
     shell('terraform apply -var "env=prod"')
     shell('terraform remote push')
